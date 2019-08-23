8f2aa8c7fd6719bc2c5a9af3493f554f188ccba8
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2019-08-23 15:46:20 +0800

    added code to find corrupt images

---
+++ find_corrupt_images.py
@@ -0,0 +1,15 @@
+import os
+from PIL import Image
+    
+wnid = 'n04045255'
+image_dir = './downloaded_images/{}/{}_urlimages'.format(wnid, wnid)
+
+for filename in os.listdir(image_dir):
+  # if filename.endswith('.png'):
+	try:
+		filepath = os.path.join(image_dir, filename)
+		img = Image.open(filepath) # open the image file
+		img.verify() # verify that it is, in fact an image
+	except (IOError, SyntaxError) as e:
+		print('Bad file, removing: {}'.format(filename)) # print out the names of corrupt files
+		os.remove(filepath)
